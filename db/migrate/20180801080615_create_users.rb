7c3ff1e989676f8552f2ef4af261d950786a056c
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-08-02 11:53:04 +0800

    Added Password digest to user migration

--- db/migrate/20180801080615_create_users.rb
+++ db/migrate/20180801080615_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,
       t.boolean :admin, default: false
       t.string :avatar
+      t.string :password_digest
       t.timestamps
     end
   end
