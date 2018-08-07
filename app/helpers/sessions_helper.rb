93755892d69624798653f1a8138512b7ce762640
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-08-07 18:29:19 +0800

    Edit user profile

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,11 +8,15 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def logged_in?
     !current_user.nil?
   end
 
-  def logout(user)
+  def logout
     session.delete(:user_id)
     current_user = nil
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  def user_avatar(user, classes)
+    @avatar = user.avatar
+    if @avatar.present?
+      @avatar_user = image_tag(@avatar.url, alt: user.name, class: classes)
+    else
+      @avatar_user = image_tag("/images/avatar.jpg", alt: user.name, class: classes)
+    end
+    return @avatar_user
+  end
 end
