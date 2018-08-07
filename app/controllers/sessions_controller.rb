d02abb19a36d235532ee6ae9be242b4e05ec60a5
Ivan Ray M. Buglosa <ivanraybuglosa@gmail.com>
2018-08-07 11:56:25 +0800

    Signin function (#4)
    * Signin function
    
    * Fixed indentions and variables
    
    * Fixed variables

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        login user
+        redirect_to root_url
+      else
+        flash[:notice] = 'Invalid User Credentials'
+        render 'new'
+      end
+  end
+  
+  def destroy
+    logout @user
+    redirect_to root_path
+  end
+  
+end
