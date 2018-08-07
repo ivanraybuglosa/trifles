09e5f5dda21c1139dbd37b05f65709e3bd21666f
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-08-07 11:53:38 +0800

    Fixed variables

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-      if user && @user.authenticate(params[:session][:password])
+      if user && user.authenticate(params[:session][:password])
         login user
         redirect_to root_url
       else
