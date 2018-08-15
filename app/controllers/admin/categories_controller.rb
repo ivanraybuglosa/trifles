41a4e00899ab0559d6ef2c93cf392bdf7cfafef9
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-08-15 17:03:29 +0800

    Category crud

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,20 +1,44 @@
 class Admin::CategoriesController < AdminController
   def new
-    @categories = Category.new
+    @category = Category.new
   end
 
   def create
-
+    @category = Category.new(category_params)
+      if @category.save
+        flash[:notice] = 'A new category has been added.' 
+        redirect_to admin_categories_url
+      else
+        render 'new'
+      end
   end
   
   def index
-    @categories = Category.all.page(params[:page]).per(5)
+    @categories = Category.all.page(params[:page]).per(5).order(created_at: :desc)
   end
 
   def destroy
+    @category = Category.find(params[:id]).destroy
+    redirect_to admin_categories_url
   end
 
   def edit
-    @categories = Category.find(params[:id])
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find_by(id: params[:id])
+      if @category.update_attributes(category_params)
+        flash[:notice] = 'Successfully updated category.'
+        redirect_to admin_categories_url
+      else
+        render 'edit'
+      end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title, :description)
   end
 end
