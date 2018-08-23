8f8fb4bd739e16f4b17f737b66886705615a2b41
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-08-23 09:53:12 +0800

    Correct choice validation

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,12 +3,23 @@ class Word < ApplicationRecord
   
   has_many :choices, dependent: :destroy
   accepts_nested_attributes_for :choices
-
+  
   validates :content,presence: true, length: { maximum: 20 }
   validates :category_id, presence: true
-
+  validate :check_choice
 
   def correct_choice
     choices.find_by(correct: true)
+  end
+
+  private
+
+  def check_choice
+    choice = choices.collect { |c| c.correct || nil }.compact
+      if choice.size == 0 
+        errors.add(:choices_correct, "can't be blank")
+      elsif choice.size > 1
+        errors.add(:choices_correct, "can have only one choice")
+      end
   end
 end
