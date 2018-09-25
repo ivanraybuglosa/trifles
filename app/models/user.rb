d23eeb799737e41dbfd4c6605a53ad4bd045e373
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-09-25 10:23:26 +0800

    Removed current_user helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -75,7 +75,7 @@ class User < ApplicationRecord
   end
 
   def remember
-    remember_token = User.new_token
+    self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
