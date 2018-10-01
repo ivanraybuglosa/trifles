57785cb98ac3350a64c0b1d2ca4d69f4f2903351
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-10-01 10:54:01 +0800

    Category-Status-filter

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,18 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.includes(:words).where.not(words: { category_id: nil })
-                          .page(params[:page]).per(6).order(created_at: :desc)
-    @lesson = Lesson.new
+    if(params[:status].nil?)
+      @categories = Category.joins(:words).where.not(words: { category_id: nil })
+                            .page(params[:page]).per(6).order(created_at: :desc)
+      @status = params[:status]
+    elsif(params[:status] == 'learned')
+      @categories = Category.joins(:lessons).where.not(lessons: { result: nil })
+                            .page(params[:page]).per(6).order(created_at: :desc)
+      @status = params[:status]
+    elsif(params[:status] == 'unlearned')
+      @categories = Category.includes(:lessons, :words).where.not(words: { category_id: nil }).where(lessons: { result: nil })
+                            .page(params[:page]).per(6).order(created_at: :desc)
+      @status = params[:status]
+    end
+    @lesson = Lesson.new  
   end
 end
