caf826a2132a3a2e485e39a0e01ca43aab313e15
Ivan Ray M. Buglosa <ivanraybuglosa@gmail.com>
2018-10-02 12:00:32 +0800

    Category-Status-filter (#23)
    * Category-Status-filter
    
    * Refactor index method for categories
    
    * Refactor index method 2.0

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,15 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.includes(:words).where.not(words: { category_id: nil })
-                          .page(params[:page]).per(6).order(created_at: :desc)
-    @lesson = Lesson.new
+    @status = params[:status]
+    if (@status.nil?)
+      categories = Category.joins(:words).where.not(words: { category_id: nil })
+    elsif (@status == 'learned')
+      categories = Category.joins(:lessons).where.not(lessons: { result: nil })
+    elsif (@status == 'unlearned')
+      categories = Category.includes(:lessons, :words).where.not(words: { category_id: nil })
+                            .where(lessons: { result: nil })
+    end
+    @categories = categories.group(:id).page(params[:page]).per(6).order(created_at: :desc)
+    @lesson = Lesson.new  
   end
 end
