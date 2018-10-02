b6bbaa33240f18fbf469f6c0d4c43192672bbaa5
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-10-02 11:57:44 +0800

    Refactor index method 2.0

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,13 @@
 class CategoriesController < ApplicationController
   def index
     @status = params[:status]
-    if(@status.nil?)
+    if (@status.nil?)
       categories = Category.joins(:words).where.not(words: { category_id: nil })
-                            .page(params[:page]).per(6).order(created_at: :desc)
-    elsif(@status == 'learned')
+    elsif (@status == 'learned')
       categories = Category.joins(:lessons).where.not(lessons: { result: nil })
-    elsif(@status == 'unlearned')
+    elsif (@status == 'unlearned')
       categories = Category.includes(:lessons, :words).where.not(words: { category_id: nil })
                             .where(lessons: { result: nil })
-                            .page(params[:page]).per(6).order(created_at: :desc)
     end
     @categories = categories.group(:id).page(params[:page]).per(6).order(created_at: :desc)
     @lesson = Lesson.new  
