8cf2490166018ec8828bfeb1e93b7782baeb2971
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-10-02 11:55:29 +0800

    Refactor index method for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,17 @@
 class CategoriesController < ApplicationController
   def index
-    if(params[:status].nil?)
-      @categories = Category.joins(:words).where.not(words: { category_id: nil })
+    @status = params[:status]
+    if(@status.nil?)
+      categories = Category.joins(:words).where.not(words: { category_id: nil })
                             .page(params[:page]).per(6).order(created_at: :desc)
-      @status = params[:status]
-    elsif(params[:status] == 'learned')
-      @categories = Category.joins(:lessons).where.not(lessons: { result: nil })
+    elsif(@status == 'learned')
+      categories = Category.joins(:lessons).where.not(lessons: { result: nil })
+    elsif(@status == 'unlearned')
+      categories = Category.includes(:lessons, :words).where.not(words: { category_id: nil })
+                            .where(lessons: { result: nil })
                             .page(params[:page]).per(6).order(created_at: :desc)
-      @status = params[:status]
-    elsif(params[:status] == 'unlearned')
-      @categories = Category.includes(:lessons, :words).where.not(words: { category_id: nil }).where(lessons: { result: nil })
-                            .page(params[:page]).per(6).order(created_at: :desc)
-      @status = params[:status]
     end
+    @categories = categories.group(:id).page(params[:page]).per(6).order(created_at: :desc)
     @lesson = Lesson.new  
   end
 end
