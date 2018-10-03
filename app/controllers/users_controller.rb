9dfbaaeb8c7520c30048388f5dca6c251a1541c0
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-10-03 11:18:29 +0800

    Words-Learned-Filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-      if user.save
+    @user = User.new(user_params)
+      if @user.save
         login user
         remember user
         redirect_to root_path
