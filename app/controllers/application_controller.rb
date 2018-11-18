75de2784af95b0472c70e1421e98be65d639a8d6
Ivan Ray Buglosa <ivanraybuglosa@gmail.com>
2018-11-18 21:17:08 +0800

    Update-Layout-02

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   
   def logged_in_user
     unless logged_in?
-    flash[:danger] = "Please log in."
-    redirect_to login_url
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
-  end
+  end   
 end
