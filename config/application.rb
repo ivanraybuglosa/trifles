0880c089837af2ad93785085a7e5c75503636914
Ivan Ray Buglosa <ivanraybuglosa@Ivans-Air.local>
2019-02-01 15:45:00 +0800

    Enabled static assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module RailsElearningIvan
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.initialize_on_precompile = false
+
+    config.serve_static_assets = true
   end
 end
