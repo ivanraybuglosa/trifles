415b290aa54428742b46f8312cd002b35c78154c
ivanraybuglosa <ivanraybuglosa@gmail.com>
2019-09-06 10:35:06 +0800

    Fixed category filter bug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,14 +2,12 @@ class CategoriesController < ApplicationController
   def index
     @status = params[:status]
     if (@status.nil?)
-      categories = Category.joins(:words).where.not(words: { category_id: nil })
+      @categories = Category.joins(:words).where.not(words: { category_id: nil }).group(:id).page(params[:page]).per(6).order(created_at: :desc)
     elsif (@status == 'learned')
-      categories = Category.joins(:lessons).where.not(lessons: { result: nil })
+      @categories = Category.joins(:lessons).where.not(lessons: { result: nil }).group(:id).page(params[:page]).per(6).order(created_at: :desc)
     elsif (@status == 'unlearned')
-      categories = Category.joins(:lessons, :words).where.not(words: { category_id: nil })
-                            .where(lessons: { result: nil })
+      @categories = Category.includes(:lessons, :words).where.not(words: { category_id: nil }).where(lessons: { result: nil }).page(params[:page]).per(6).order(created_at: :desc)
     end
-    @categories = categories.group(:id).page(params[:page]).per(6).order(created_at: :desc)
     @lesson = Lesson.new  
   end
 end
